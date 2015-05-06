09753c34c67158a72a68dd0e8cecae7d245bc1ff
Kyle McCarthy <km@kylemccarthy.io>
2015-05-06 18:38:04 -0500

    send and receive messages

--- src/Ipc.c
+++ src/Ipc.c
@@ -1,15 +1,29 @@
 #include "../include/ipc.h"
-#include "stdio.h"
-#include "stdlib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/errno.h>
+#include <string.h>
 
 #define BUFFER_SIZE 4096
+#define MSG_BUFFER 128
+#define MSG_PERM 0600
+#define KEY 6151
 
 typedef struct {
-    unsigned char* data;
     unsigned int index;
+    unsigned char data[BUFFER_SIZE];
 } buffer_t;
 
+typedef unsigned char message_buffer_t[128];
+typedef struct {
+    long type;
+    message_buffer_t data;
+} message_t;
+
 static buffer_t data_buffer;
+int errno;
 
 /**********************************************************
  * PRIVATE FUNCTION PROTOTYPES
@@ -35,19 +49,57 @@ void _init_message_queue();
 
 void init_ipc() {
     _init_buffer(&data_buffer);
+    _init_message_queue();
 }
 
 void _init_buffer(buffer_t* buffer) {
-    // set up the buffer
-    buffer->data = malloc(BUFFER_SIZE * sizeof(unsigned int));
     // Go through for i = 0 to i < 4KB and fill the buffer
+    buffer->index = 0;
     for (unsigned int i = 0; i < BUFFER_SIZE; i++) {
         buffer->data[i] = rand();
     }
 }
 
 void _init_message_queue() {
+    // create the message queue
+    int mqid = msgget(KEY, MSG_PERM|IPC_CREAT|IPC_EXCL);
 
+    // check to make sure the message was created
+    if (mqid < 0) {
+        perror(strerror(errno));
+        printf("%s\n", "ERROR: Failed to create msg queue.");
+        return;
+    }
+
+    // create a new messages and load in 128 bytes
+    message_t message;
+    message_t response;
+    message.type = 1;
+    for (unsigned int i = 0; i < 128; i++) {
+        message.data[i] = data_buffer.data[i];
+        data_buffer.index++;
+    }
+
+    // send the message
+    if (msgsnd(mqid, &message, sizeof(message_buffer_t), 0) != 0) {
+        perror(strerror(errno));
+        printf("%s\n", "ERROR: Failed to send message.");
+        return;
+    }
+
+    // get the message
+    if (msgrcv(mqid, &response, sizeof(message_buffer_t), 1, 0) != MSG_BUFFER) {
+        perror(strerror(errno));
+        printf("%s\n", "ERROR: Failed to receive message.");
+        return;
+    }
+
+    // clear the message queue
+    if (msgctl(mqid, IPC_RMID, NULL) < 0) {
+        perror(strerror(errno));
+        printf("%s\n", "ERROR: Failed to clear msg queue.");
+        return;
+    }
 }
 
 
