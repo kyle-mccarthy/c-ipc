d9bfb8c2a4005d8d232c1294a46a553e42783635
Kyle McCarthy <km@kylemccarthy.io>
2015-05-06 21:06:29 -0500

    message queue with forking

--- src/Ipc.c
+++ src/Ipc.c
@@ -5,18 +5,22 @@
 #include <sys/msg.h>
 #include <sys/errno.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define BUFFER_SIZE 4096
 #define MSG_BUFFER 128
 #define MSG_PERM 0600
-#define KEY 6151
+#define KEY 6161
 
 typedef struct {
     unsigned int index;
     unsigned char data[BUFFER_SIZE];
 } buffer_t;
 
-typedef unsigned char message_buffer_t[128];
+// need to do this so we can create a member variable inside of the message_t struct
+// instead of using a pointer
+typedef unsigned char message_buffer_t[MSG_BUFFER];
 typedef struct {
     long type;
     message_buffer_t data;
@@ -43,6 +47,18 @@ void _init_buffer(buffer_t* buffer);
  */
 void _init_message_queue();
 
+/**
+ *
+ *
+ */
+void _mq_parent(message_t* message);
+
+/**
+ *
+ *
+ */
+void _mq_child(message_t* response);
+
 /**********************************************************
  * FUNCTION IMPLEMENTATION
  **********************************************************/
@@ -68,59 +84,51 @@ void _init_message_queue() {
     if (mqid < 0) {
         perror(strerror(errno));
         printf("%s\n", "ERROR: Failed to create msg queue.");
-        return;
+        exit(1);
     }
 
-    // create a new messages and load in 128 bytes
+    pid_t pid = fork();
+    int status;
+
     message_t message;
     message_t response;
-    message.type = 1;
-    for (unsigned int i = 0; i < 128; i++) {
-        message.data[i] = data_buffer.data[i];
-        data_buffer.index++;
+
+    // child process
+    if (pid == 0) {
+        for (int c = 0; c < (BUFFER_SIZE/MSG_BUFFER); c++) {
+            if (msgrcv(mqid, &response, sizeof(message_buffer_t), 1, 0) != MSG_BUFFER) {
+                perror(strerror(errno));
+                printf("%s\n", "ERROR: Failed to receive message.");
+                exit(1);
+            }
+        }
+        exit(1);
+    } else if (pid > 0) {
+        for (int c = 0; c < (BUFFER_SIZE/MSG_BUFFER); c++) {
+            // set the message
+            message.type = 1;
+            for (int i = 0; i < MSG_BUFFER; i++) {
+                message.data[i] = data_buffer.data[i];
+                data_buffer.index++;
+            }
+            // send the message
+            if (msgsnd(mqid, &message, sizeof(message_buffer_t), 0) != 0) {
+                perror(strerror(errno));
+                printf("%s\n", "ERROR: Failed to send message.");
+                exit(1);
+            }
+        }
     }
 
-    // send the message
-    if (msgsnd(mqid, &message, sizeof(message_buffer_t), 0) != 0) {
-        perror(strerror(errno));
-        printf("%s\n", "ERROR: Failed to send message.");
-        return;
-    }
-
-    // get the message
-    if (msgrcv(mqid, &response, sizeof(message_buffer_t), 1, 0) != MSG_BUFFER) {
-        perror(strerror(errno));
-        printf("%s\n", "ERROR: Failed to receive message.");
-        return;
-    }
+    waitpid(pid, &status, WUNTRACED | WCONTINUED);
 
     // clear the message queue
     if (msgctl(mqid, IPC_RMID, NULL) < 0) {
         perror(strerror(errno));
         printf("%s\n", "ERROR: Failed to clear msg queue.");
-        return;
+        exit(1);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
